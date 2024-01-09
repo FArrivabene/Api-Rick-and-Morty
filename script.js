a0f2956910d0f18ac44ce3e6f3f80c138cfa98ba
Fabio Arrivabene <107645610+FArrivabene@users.noreply.github.com>
2024-01-09 13:52:02 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+const characterId = document.getElementById("characterId");
+const btnGo = document.getElementById("btn-go");
+const btnReset = document.getElementById("btn-reset");
+const content = document.getElementById("content");
+const conteinerResult = document.getElementById("result-style");
+const image = document.getElementById("img");
+
+const fetchApi = (value) => {
+  const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+  return result;
+};
+
+const keys = ["name", "status", "species", "gender", "origin", "episode"];
+
+const buildResult = (result) => {
+  const newObjetct = {};
+  keys
+    .map((key) => document.getElementById(key))
+    .map((elem) => {
+      elem.checked && (newObjetct[elem.name] = result[elem.name]);
+    });
+
+  return newObjetct;
+};
+
+const newKeys = {
+  name: "Nome",
+  status: "Status",
+  species: "Espécie",
+  gender: "Gênero",
+  origin: "Planeta de origem",
+  episode: "Episódios",
+};
+
+btnGo.addEventListener("click", async (event) => {
+  event.preventDefault();
+  const result = await fetchApi(characterId.value);
+  //content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+  content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
+  console.log(buildResult(result));
+  image.src = `${result.image}`;
+});
